c9103a79e08795da421db66fb3e4eb5e7e1148e8
Katarina Cheng <kacheng2017@kacheng2017.local>
2016-10-11 12:47:57 -0700

    commit

--- 2-the-fizzbuzz-event/app.js
+++ 2-the-fizzbuzz-event/app.js
@@ -1,6 +1,49 @@
-// Your Code Here:
-function fizzBuzz(n) {
-  // Implement FizzBuzz
+$(document).ready(function(){
+let numbers = [];
+
+function custom(t,n,m) {
+  if (t%(n*m) == 0) {
+    return("FizzBuzz! <br />");
+  } else if (t%n == 0) {
+   return("Fizz! <br />");
+  } else if (t%m == 0) {
+    return("Buzz! <br />");
+  } else {
+    return(t + " <br />");
+  }
 }
 
-// Any other functions your app requires
+function fizzBuzz(n,m) {
+  for(var t = 1; t < 100; t++){
+  $("#fizz-buzz").append(custom(t,n,m));
+  }
+}
+
+class NumberDiv {
+  constructor(number) {
+    this.number = number;
+  }
+}
+
+class Number {
+  constructor(number_1,number_2) {
+    this.number_1 = number_1;
+    this.number_2 = number_2;
+  }
+}
+
+
+function addNumber(number) {
+  numbers.push(number);
+}
+
+$("#new-number").submit( (e) => {
+  e.preventDefault();
+  const number_1 = $("#number_1").val();
+  const number_2 = $("#number_2").val();
+  const newNumber = new
+  Number(number_1,number_2);
+  addNumber(newNumber);
+  fizzBuzz(number_1,number_2);
+});
+});
